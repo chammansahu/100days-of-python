37435b591586afc33fa22b90904834d8e43939c0
chammansahu <chammansahu@gmail.com>
2021-07-03 10:57:42 +0530

    calculator project done!!!

--- day 10 function with outputs/final project -Calculator.py
+++ day 10 function with outputs/final project -Calculator.py
@@ -1,4 +1,53 @@
 from art import logo
 
 
-print(logo)
+
+def addition(op1,op2):
+    return op1 + op2
+
+def substraction(op1,op2):
+    return op1 - op2
+
+def multiplication(op1,op2):
+    return op1 * op2
+
+def division(op1,op2):
+    return op1 / op2
+
+operations={
+    "+":addition,
+    "-":substraction,
+    "*":multiplication,
+    "/":division,
+}
+def calculator():
+    print(logo)  
+    
+    num1 = float(input("Enter Number One: "))
+    for symbol in operations:
+   
+        print(f" {symbol}")
+    should_continue = True
+    
+    while should_continue:
+            
+        user=input("pick a symbol : ")
+        num2 = float(input("Enter Number Two: "))
+
+        answer = operations[user](num1,num2)
+
+        print(f"{num1} {user} {num2} = {answer}")
+        
+        moreOperation=input("Do you want to calculate another?Yes Or No  ")
+        
+        if(moreOperation=="yes"):
+            num1=answer
+            calculator()
+        elif (moreOperation == "no"):
+            print(f"{num1} {user} {num2} = {answer}")
+            should_continue=False;
+            
+        else:
+            print("you enterd wrong details")        
+            
+calculator()        
